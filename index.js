1a20ef19be7c7a33d050bfe04fa678d6a11538be
Alamin-git <alamin2.mas@gmail.com>
2022-05-16 15:50:43 +0600

    cShutter server ready

--- index.js
+++ index.js
@@ -34,6 +34,21 @@ async function run() {
         res.send(service);   
     })
 
+    // post service
+    app.post('/service', async(req,res) =>{
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    })
+
+    // Delete service 
+    app.delete('/service/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+  })
+
   } finally {
   }
 }
